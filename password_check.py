bd0b1ecdfaf463501e5eb6b2838ca4ba4440c17f
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-02 22:17:29 +0800

    Add docstring to password checker

--- password_check.py
+++ password_check.py
@@ -1,5 +1,11 @@
+"""
+Description: Simple password checker
+Name: Joshua Timothy Gratio Wibowo
+"""
+
 PASS_MIN_LENGTH = 5
 user_password = input(f"Enter {PASS_MIN_LENGTH} char password: ")
+
 while len(user_password) != PASS_MIN_LENGTH:
     print(f"Invalid password, must be {PASS_MIN_LENGTH} char long")
     user_password = input(f"Enter {PASS_MIN_LENGTH} char password: ")
