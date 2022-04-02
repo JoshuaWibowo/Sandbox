7940e65b2a0f98eaa8caf9e99838daea52bff72b
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-02 22:14:53 +0800

    Add a password checking function

---
+++ password_check.py
@@ -0,0 +1,8 @@
+PASS_MIN_LENGTH = 5
+user_password = input(f"Enter {PASS_MIN_LENGTH} char password: ")
+while len(user_password) != PASS_MIN_LENGTH:
+    print(f"Invalid password, must be {PASS_MIN_LENGTH} char long")
+    user_password = input(f"Enter {PASS_MIN_LENGTH} char password: ")
+
+for char in user_password:
+    print("*", end='')
